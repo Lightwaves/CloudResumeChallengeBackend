e2356a2cd735fa8060de111c974759bb2923841b
Stephen Ramos <storamos@gmail.com>
2021-12-07 00:11:18 -0500

    Add project files.

---
+++ CloudResumeChallenge.Tests/Features/DBConnect.feature
@@ -0,0 +1,8 @@
+﻿Feature: DBConnect
+
+A short summary of the feature
+
+
+Scenario: Successfully Connecting to a Database
+	Given a connection string
+	Then I should successfully connect to a database
---
+++ CloudResumeChallenge.Tests/StepDefinitions/DBConnectSteps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+using Npgsql;
+
+namespace CloudResumeChallenge.Tests.StepDefinitions
+{
+
+    [Binding]
+    public sealed class DBConnectSteps
+    {
+
+
+
+        [Given (@"a connection string")]
+
+        public void GivenIConnectToDB() { 
+            
+        
+        
+        }
+
+    }
+}
